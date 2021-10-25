077dcdd3dc0edf6de0f7dbdcc93c5ef1a0761948
watabusi <92659582+watabusi@users.noreply.github.com>
2021-10-25 14:27:50 +0900

    topの更新ファイル
    ts,html,cssすべて

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Memo } from '../Memo';
+import { MemoService } from '../memo.service';
+
 @Component({
   selector: 'app-top',
   templateUrl: './top.component.html',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TopComponent implements OnInit {
 
-  constructor() { }
+  title = "メモ管理アプリ";
+
+  memos: Memo[] = [];
+
+  constructor(private memoService: MemoService) { }
 
   ngOnInit(): void {
+    this.getMemosTop5();
+  }
+
+  getMemosTop5(): void {
+    this.memoService.getMemos().subscribe(memos => this.memos = memos.slice(0, 5));
   }
 
 }
