77267ab2f9903a95684568e58f30586dd15b11c5
watabusi <92659582+watabusi@users.noreply.github.com>
2021-10-25 14:35:38 +0900

    19日分から
    htmlとts

--- src/app/top/top.component.ts
+++ src/app/top/top.component.ts
@@ -20,7 +20,9 @@ export class TopComponent implements OnInit {
     this.getMemosTop5();
   }
 
+  /** 直近に編集されたメモ5つを取得 */
   getMemosTop5(): void {
+    // DB側で並べ替えた後に取得するので、こちらでは並び替えられた上位5つだけ抽出すればいい
     this.memoService.getMemos().subscribe(memos => this.memos = memos.slice(0, 5));
   }
 
