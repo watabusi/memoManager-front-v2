2e1787f7809a58231b99ffacb9ce15ab9f24b023
watabusi <watabusi@gmail.com>
2021-10-25 16:19:38 +0900

    中間テーブルが以前のまま、memoIdがすべて１になっておりソートがバグってたので修正

--- src/app/service-memotag/mock-memotag.ts
+++ src/app/service-memotag/mock-memotag.ts
@@ -1,8 +1,8 @@
-import { MemoTag } from "./memo-tag";
+import { MemoTag } from './memo-tag';
 
 export const MEMOTAGS: MemoTag[] = [
-    { id: 1, memoId: 1, tagId: 1 },
-    { id: 2, memoId: 1, tagId: 1 },
-    { id: 3, memoId: 1, tagId: 1 },
-    { id: 4, memoId: 1, tagId: 1 },
-]
+  { id: 1, memoId: 1, tagId: 1 },
+  { id: 2, memoId: 2, tagId: 1 },
+  { id: 3, memoId: 3, tagId: 1 },
+  { id: 4, memoId: 4, tagId: 1 },
+];
