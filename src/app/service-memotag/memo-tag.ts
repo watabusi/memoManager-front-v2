8b367aaf3896ef4077fb76c5752060f2a712249b
watabusi <92659582+watabusi@users.noreply.github.com>
2021-10-26 17:02:26 +0900

    余計なmemotag削除

--- src/app/service-memotag/memo-tag.ts
+++
@@ -1,5 +0,0 @@
-export interface MemoTag {
-    id: number;
-    memoId: number;
-    tagId: number;
-}
--- src/app/service-memotag/mock-memotag.ts
+++
@@ -1,8 +0,0 @@
-import { MemoTag } from "./memo-tag";
-
-export const MEMOTAGS: MemoTag[] = [
-    { id: 1, memoId: 1, tagId: 1 },
-    { id: 2, memoId: 1, tagId: 1 },
-    { id: 3, memoId: 1, tagId: 1 },
-    { id: 4, memoId: 1, tagId: 1 },
-]
