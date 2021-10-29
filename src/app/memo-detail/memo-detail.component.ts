6de2f4a14178372afe60f5486d7abdbb66cf03a8
watabusi <watabusi@gmail.com>
2021-10-29 11:58:19 +0900

    memoDetail-ts urlからidを取得し該当のメモを探すgetMemo()の追加と、それに必要なファイルのインポート

--- src/app/memo-detail/memo-detail.component.ts
+++ src/app/memo-detail/memo-detail.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { Memo } from '../service-memo/memo';
+import { MemoService } from '../service-memo/memo.service';
 
 @Component({
   selector: 'app-memo-detail',
   templateUrl: './memo-detail.component.html',
-  styleUrls: ['./memo-detail.component.css']
+  styleUrls: ['./memo-detail.component.css'],
 })
 export class MemoDetailComponent implements OnInit {
+  constructor(
+    private route: ActivatedRoute,
+    private memoService: MemoService,
+    private location: Location
+  ) {}
 
-  constructor() { }
+  memo: Memo | undefined;
 
   ngOnInit(): void {
+    this.getMemo();
   }
 
+  getMemo(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.memoService.getMemo(id).subscribe((memo) => (this.memo = memo));
+  }
 }
